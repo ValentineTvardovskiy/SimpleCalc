153433de7946a4fbc4dbefc02b82981d2958c717
Valentine Tvardovskiy <valentintw1999@gmail.com>
2018-05-10 16:41:44 +0300

    start with Jenkins

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public int getSum(int x, int y) {
+        return x+y;
+    }
+
+    public int getDivide(int x, int y) {
+        return x/y;
+    }
+
+    public int getMultiple(int x, int y) {
+        return x*y;
+    }
+}
